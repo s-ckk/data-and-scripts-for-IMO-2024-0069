258cdaa5fc73ad356d4f2cae0575e2682a7f8146
s-ckk <161600401+s-ckk@users.noreply.github.com>
2024-09-12 21:10:57 +0800

    Add files via upload

---
+++ figure2/figure2A.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(pheatmap)
+data <- read.table("figure2A.tab", header = TRUE, row.names = 1)
+data <- as.matrix(data)
+pheatmap(data, 
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         show_colnames = FALSE, show_rownames = FALSE, cellwidth = 0.1, cellheight = 8, color = colorRampPalette(c("white", "#8693cf"))(100))
