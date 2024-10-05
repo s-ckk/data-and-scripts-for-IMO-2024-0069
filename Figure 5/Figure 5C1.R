5ea705b07a3a3b3d9731b9077d214dabb01f2c70
s-ckk <161600401+s-ckk@users.noreply.github.com>
2024-10-05 18:36:37 +0800

    Add files via upload

---
+++ Figure 5/Figure 5C1.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+library(pheatmap)
+data = readr::read_tsv("Figure 5C1.tsv")
+data <- as.matrix(data)
+rownames(data) <- data[, 1]
+data <- data[, -1]
+write.table(data, "data.tab", sep="\t", quote=FALSE)
+data <- read.table("data.tab")
+pheatmap(data,
+         cluster_rows = FALSE,
+         cluster_cols = FALSE,
+         show_colnames = FALSE, show_rownames = FALSE, cellwidth = 0.05, cellheight = 4, color = colorRampPalette(c("#ecf1f0", "#6e938f"))(100))
