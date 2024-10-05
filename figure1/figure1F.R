05cd0e66c5d710d5ac09faee699fd32d18268535
s-ckk <1351332788@qq.com>
2024-10-05 18:44:35 +0800

    Delete file

--- figure1/figure1F.R
+++
@@ -1,9 +0,0 @@
-library(ggplot2)
-library(pheatmap)
-setwd("C:/Users/admin/Desktop/data and scripts/figure1/")
-data <- read.table("figure1F.tab", header = TRUE, row.names = 1)
-data <- as.matrix(data)
-pheatmap(data, 
-         cluster_rows = TRUE,
-         cluster_cols = TRUE,
-         show_colnames = FALSE, show_rownames = FALSE, cellwidth = 0.02, cellheight = 1, color = colorRampPalette(c("white", "#8693cf"))(100))
